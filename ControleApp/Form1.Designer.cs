a7c8bd43687f12da6aa9903a30c140c51a83406c
mateusap <ma.aparecido.silva@hotmail.com>
2023-07-23 20:14:22 -0300

    Criação do projeto e primeiros passos

---
+++ ControleApp/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ControleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(374, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(198, 109);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(248, 214);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "CÓDIGO";
+            this.columnHeader1.Width = 107;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "NOME";
+            this.columnHeader2.Width = 200;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(34, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 199);
+            this.listBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(234, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 335);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ControleApp/Form1.cs
@@ -0,0 +1,105 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleApp
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_NCLBUTTODOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        static string connstring = "server=localhost;userid=root;password=;database=new_schema";
+        MySqlConnection connection = new MySqlConnection(connstring);
+        private static ListView listview;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            using (var conn = new MySqlConnection(connstring))
+            {
+                conn.Open();
+                DataTable dt = new DataTable();
+                MySqlCommand command = new MySqlCommand("SELECT * FROM new_schema.pasta1", conn);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                adapter.SelectCommand = command;
+                adapter.Fill(dt);
+
+                listBox1.DataSource = dt;
+                listBox1.DisplayMember = "nome";
+                listBox1.ValueMember = "nome";
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    DataRow row = dt.Rows[i];
+                    ListViewItem item = new ListViewItem(row["código"].ToString());
+                    item.SubItems.Add(row["nome"].ToString());
+
+                    listView1.Items.Add(item);
+                }
+                conn.Close();
+
+
+            }
+        }
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            connection.Open();
+            string uf;
+            string nome = listView1.SelectedItems[0].SubItems[1].Text;
+            MySqlCommand command = new MySqlCommand($"SELECT * FROM new_schema.pasta1 WHERE nome = '{nome}'", connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                uf = reader["uf"].ToString();
+
+                textBox1.Text = uf;
+                reader.Close();
+            }
+            else
+            {
+                reader.Close();
+                MessageBox.Show("erro na busca");
+            }
+            connection.Close();
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+            ListViewItem item = listView1.FindItemWithText(textBox2.Text, true, 0);
+            if (item != null) { item.Selected = true; }
+            
+        }
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ListViewItem item = listView1.FindItemWithText(textBox2.Text, true, 0);
+                if (item != null) { item.Selected = true;}
+
+            }
+        }
+    }
+}
